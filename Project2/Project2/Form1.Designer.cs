4d8ccb904e578715dc0ec770364803f8d5f45e01
jkbrow08 <jkbrow08@cardmail.louisville.edu>
2016-11-28 21:40:26 -0500

    Add files via upload

---
+++ Project2/Project2/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Project2
+{
+    partial class RegistrationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.creditHourLbl = new System.Windows.Forms.Label();
+            this.creditHourTxt = new System.Windows.Forms.TextBox();
+            this.lastNamelbl = new System.Windows.Forms.Label();
+            this.lastNameTxt = new System.Windows.Forms.TextBox();
+            this.calculteLbl = new System.Windows.Forms.Label();
+            this.calculateBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // creditHourLbl
+            // 
+            this.creditHourLbl.AutoSize = true;
+            this.creditHourLbl.Location = new System.Drawing.Point(62, 26);
+            this.creditHourLbl.Name = "creditHourLbl";
+            this.creditHourLbl.Size = new System.Drawing.Size(128, 26);
+            this.creditHourLbl.TabIndex = 5;
+            this.creditHourLbl.Text = "Please Enter your number\r\nof credit hours.";
+            this.creditHourLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // creditHourTxt
+            // 
+            this.creditHourTxt.Location = new System.Drawing.Point(109, 83);
+            this.creditHourTxt.Name = "creditHourTxt";
+            this.creditHourTxt.Size = new System.Drawing.Size(34, 20);
+            this.creditHourTxt.TabIndex = 6;
+            this.creditHourTxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lastNamelbl
+            // 
+            this.lastNamelbl.AutoSize = true;
+            this.lastNamelbl.Location = new System.Drawing.Point(209, 26);
+            this.lastNamelbl.Name = "lastNamelbl";
+            this.lastNamelbl.Size = new System.Drawing.Size(90, 26);
+            this.lastNamelbl.TabIndex = 7;
+            this.lastNamelbl.Text = "Please Enter your\r\n last name.";
+            this.lastNamelbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lastNameTxt
+            // 
+            this.lastNameTxt.Location = new System.Drawing.Point(205, 83);
+            this.lastNameTxt.Name = "lastNameTxt";
+            this.lastNameTxt.Size = new System.Drawing.Size(98, 20);
+            this.lastNameTxt.TabIndex = 8;
+            this.lastNameTxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // calculteLbl
+            // 
+            this.calculteLbl.AutoSize = true;
+            this.calculteLbl.Location = new System.Drawing.Point(46, 119);
+            this.calculteLbl.Name = "calculteLbl";
+            this.calculteLbl.Size = new System.Drawing.Size(160, 39);
+            this.calculteLbl.TabIndex = 9;
+            this.calculteLbl.Text = "Press the Calculate button to\r\n determine your registration date \r\nand time";
+            this.calculteLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // calculateBtn
+            // 
+            this.calculateBtn.Location = new System.Drawing.Point(89, 176);
+            this.calculateBtn.Name = "calculateBtn";
+            this.calculateBtn.Size = new System.Drawing.Size(75, 23);
+            this.calculateBtn.TabIndex = 10;
+            this.calculateBtn.Text = "Calculate";
+            this.calculateBtn.UseVisualStyleBackColor = true;
+            this.calculateBtn.Click += new System.EventHandler(this.calculateBtn_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(217, 176);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(75, 23);
+            this.clearBtn.TabIndex = 11;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // RegistrationForm
+            // 
+            this.AcceptButton = this.calculateBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(359, 221);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.calculateBtn);
+            this.Controls.Add(this.calculteLbl);
+            this.Controls.Add(this.lastNameTxt);
+            this.Controls.Add(this.lastNamelbl);
+            this.Controls.Add(this.creditHourTxt);
+            this.Controls.Add(this.creditHourLbl);
+            this.Name = "RegistrationForm";
+            this.Text = "Registration Date and Time Determination";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label creditHourLbl;
+        private System.Windows.Forms.TextBox creditHourTxt;
+        private System.Windows.Forms.Label lastNamelbl;
+        private System.Windows.Forms.TextBox lastNameTxt;
+        private System.Windows.Forms.Label calculteLbl;
+        private System.Windows.Forms.Button calculateBtn;
+        private System.Windows.Forms.Button clearBtn;
+    }
+}
+
---
+++ Project2/Project2/Form1.cs
@@ -0,0 +1,227 @@
+﻿//Name:    Jeremy Brown
+//Date:    10/21/2011
+//Class:   CIS 199-01
+//Purpose: The purpose of this program is to determine the date and time of registration for classes
+//         for new and continuing students for the upcoming school semester.
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Project2
+{
+    public partial class RegistrationForm : Form
+    {
+        public RegistrationForm()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateBtn_Click(object sender, EventArgs e)
+        {
+            double hours;//Used to store the hours entered by the user
+            double time;//Used to store and display the time of registration based on the user's input
+            string date ;//Used to store and display the date of registration based on the user's input
+            string lastName;//Used to store the last name of the user
+            string messageBox;//Used to store the message that will later be outputted by the message box.
+            char firstLetter;//Used to store the first letter of the last name in char format
+            const double SENIOR = 90;//used to store the constant of how many hours is considered a senior
+            const double JUNIOR = 60;//used to store the constant of how many hours is considered a junior
+            const double SOPHMORE = 30;//used to store the constant of how many hours is considered a sophomore
+            const string SENIORDATE = "11/07/2011";//Used to store the senior date of registration
+            const string JUNIOR_DATE = "11/08/2011";//Used to store the junior date of registration
+            const string SOPHOMORE_DATE_1 = "11/09/2011";//Used to store the first sophomore date of registration
+            const string SOPHOMORE_DATE_2 = "11/10/2011";//Used to store the second sophomore date of registration
+            const string FRESHMAN_DATE_1 = "11/11/2011";//Used to store the first freshman date of registration
+            const string FRESHMAN_DATE_2 = "11/14/2011";//Used to store the second freshman date of registration
+            hours = Convert.ToInt32(creditHourTxt.Text);
+            lastName = Convert.ToString(lastNameTxt.Text);
+            lastName = lastName.Substring(0, 1);
+            lastName = lastName.ToLower();
+            firstLetter = Convert.ToChar(lastName);
+
+            if (hours >= SENIOR)//Determines if the user is a senior
+                if (firstLetter >= 'e' && firstLetter <= 'i')
+                {
+                    time = 8;
+                    date = SENIORDATE;
+                }
+                else if (firstLetter >= 'j' && firstLetter <= 'o')
+                {
+                    time = 10;
+                    date = SENIORDATE;
+                }
+                else if (firstLetter >= 'p' && firstLetter <= 't')
+                {
+                    time = 12;
+                    date = SENIORDATE;
+                }
+                else if (firstLetter >= 'u' && firstLetter <= 'z')
+                {
+                    time = 2;
+                    date = SENIORDATE;
+                }
+
+                else
+                {
+                    time = 4;
+                    date = SENIORDATE;
+                }
+
+
+            else if (hours >= JUNIOR)//Determines if the user is a junior
+                if (firstLetter >= 'e' && firstLetter <= 'i')
+                {
+                    time = 8;
+                    date = JUNIOR_DATE;
+                }
+                else if (firstLetter >= 'j' && firstLetter <= 'o')
+                {
+                    time = 10;
+                    date = JUNIOR_DATE;
+                }
+                else if (firstLetter >= 'p' && firstLetter <= 't')
+                {
+                    time = 12;
+                    date = JUNIOR_DATE;
+                }
+                else if (firstLetter >= 'u' && firstLetter <= 'z')
+                {
+                    time = 2;
+                    date = JUNIOR_DATE;
+                }
+
+                else
+                {
+                    time = 4;
+                    date = JUNIOR_DATE;
+                }
+            else if (hours >= SOPHMORE)//Determines if the user is a sophomore
+                if (firstLetter >= 'e' && firstLetter <= 'f')
+                {
+                    time = 8;
+                    date = SOPHOMORE_DATE_1;
+                }
+                else if (firstLetter >= 'g' && firstLetter <= 'i')
+                {
+                    time = 10;
+                    date = SOPHOMORE_DATE_1;
+                }
+                else if (firstLetter >= 'j' && firstLetter <= 'l')
+                {
+                    time = 12;
+                    date = SOPHOMORE_DATE_1;
+                }
+                else if (firstLetter >= 'm' && firstLetter <= 'o')
+                {
+                    time = 2;
+                    date = SOPHOMORE_DATE_1;
+                }
+
+                else if (firstLetter >= 'p' && firstLetter <= 'r')
+                {
+                    time = 4;
+                    date = SOPHOMORE_DATE_1;
+                }
+                else if (firstLetter >= 's' && firstLetter <= 't')
+                {
+                    time = 8;
+                    date = SOPHOMORE_DATE_2;
+                }
+                else if (firstLetter >= 'u' && firstLetter <= 'v')
+                {
+                    time = 10;
+                    date = SOPHOMORE_DATE_2;
+                }
+                else if (firstLetter >= 'w' && firstLetter <= 'z')
+                {
+                    time = 12;
+                    date = SOPHOMORE_DATE_2;
+                }
+                else if (firstLetter >= 'a' && firstLetter <= 'b')
+                {
+                    time = 2;
+                    date = SOPHOMORE_DATE_2;
+                }
+                else 
+                {
+                    time = 8;
+                    date = SOPHOMORE_DATE_2;
+                }
+
+            else//If the higher conditions are not met then the user is seen as a freshman
+                if (firstLetter >= 'e' && firstLetter <= 'f')
+                {
+                    time = 8;
+                    date = FRESHMAN_DATE_1;
+                }
+                else if (firstLetter >= 'g' && firstLetter <= 'i')
+                {
+                    time = 10;
+                    date = FRESHMAN_DATE_1;
+                }
+                else if (firstLetter >= 'j' && firstLetter <= 'l')
+                {
+                    time = 12;
+                    date = FRESHMAN_DATE_1;
+                }
+                else if (firstLetter >= 'm' && firstLetter <= 'o')
+                {
+                    time = 2;
+                    date = FRESHMAN_DATE_1;
+                }
+
+                else if (firstLetter >= 'p' && firstLetter <= 'r')
+                {
+                    time = 4;
+                    date = FRESHMAN_DATE_1;
+                }
+                else if (firstLetter >= 's' && firstLetter <= 't')
+                {
+                    time = 8;
+                    date = FRESHMAN_DATE_2;
+                }
+                else if (firstLetter >= 'u' && firstLetter <= 'v')
+                {
+                    time = 10;
+                    date = FRESHMAN_DATE_2;
+                }
+                else if (firstLetter >= 'w' && firstLetter <= 'z')
+                {
+                    time = 12;
+                    date = FRESHMAN_DATE_2;
+                }
+                else if (firstLetter >= 'a' && firstLetter <= 'b')
+                {
+                    time = 2;
+                    date = FRESHMAN_DATE_2;
+                }
+                else
+                {
+                    time = 8;
+                    date = FRESHMAN_DATE_2;
+                }
+
+            //creates the message box string, and then displays it in a message box
+            messageBox = string.Format("Your first day of eligibility for class reigistration is on {0} at {1}:00", date, time);
+            MessageBox.Show(messageBox, "Registration Date and Time");
+
+
+
+
+
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            //Clears the credit hour and last name text boxes, and focus on the credit hour text box
+            creditHourTxt.Clear();
+            lastNameTxt.Clear();
+            creditHourTxt.Focus();
+        }
+    }
+}
